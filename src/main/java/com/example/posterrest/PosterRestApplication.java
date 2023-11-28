286a4bbfe20aed42ca411b31e3f5fcf5a8e417a9
Alesha1229 <ale05xi@gmail.com>
2023-11-29 00:06:31 +0300

    Initial commit

---
+++ src/main/java/com/example/posterrest/PosterRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.posterrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosterRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosterRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/posterrest/PosterRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.posterrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosterRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
