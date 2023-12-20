ed4e844c07c91267df4f2c19f465d8620c414907
Alesha1229 <ale05xi@gmail.com>
2023-12-20 16:22:20 +0300

    Final commit

---
+++ src/main/java/com/example/posterrest/configuration/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.example.posterrest.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+  private final JWTTokenProvider jwtTokenProvider;
+
+  private static final String ADMIN_ENDPOINT = "/api/admin/**";
+
+
+  private static final String[] PUBLIC_URLS = {
+      "/v2/api-docs",
+      "/swagger-ui/index.html",
+      "/swagger-resources/**",
+      "configuration/**",
+      "webjars/**",
+      "/*.html",
+      "/**/*.html",
+      "/**/*.css",
+      "/**/*.js",
+          "/events/getAll"
+  };
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .csrf()
+        .disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .antMatchers(ADMIN_ENDPOINT).hasAuthority("ADMIN")
+        .antMatchers(HttpMethod.GET, PUBLIC_URLS).permitAll()
+        .antMatchers("/db/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .apply(new JWTConfig(jwtTokenProvider));
+    http
+        .headers().frameOptions().sameOrigin();
+  }
+}
